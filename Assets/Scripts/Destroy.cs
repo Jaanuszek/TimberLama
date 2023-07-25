377425b2ec11b029d13df93e5e98271fa4441e5b
Jaanuszek <kubex564@gmail.com>
2023-07-25 16:32:08 +0200

    uff
    Deleted some unneccesary code and comments, added function that can destroy appropriate objects on proper button click. And i am really proud of myself XD

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -7,53 +7,11 @@ public class Destroy : MonoBehaviour
 {
     void Update()
     {
-        String leftObj = "Left(Clone)";
-        String rightObj = "Right(Clone)";
-        GameObject left = GameObject.Find(leftObj);
-        GameObject right = GameObject.Find(rightObj);
-        if (Input.GetKeyDown("left"))
-        {
-            if (left)
-            {
-                Destroy(left.gameObject);
-            }
-            //if (right)
-            //{
-            //    Debug.Log("GameOver");
-            //}
-        }
-        if (Input.GetKeyDown("right"))
-        {
-            if (right)
-            {
-                Destroy(right.gameObject);
-            }
-        }
+
     }
 
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        //string[] myObjectsNames = new string[] { "Left(Clone)", "Right(Clone)" };
-        //foreach(string myObjectName in myObjectsNames)
-        //{
-        //    GameObject go = GameObject.Find(myObjectName);
-        //    if (go)
-        //    {
-        //        Destroy(go.gameObject);
-        //    }
-        //}
-        //String leftObj = "Left(Clone)";
-        //String rightObj = "Right(Clone)";
-        //GameObject left = GameObject.Find(leftObj);
-        //GameObject right = GameObject.Find(rightObj);
-        //if (left)
-        //{
-        //    Destroy(left.gameObject);
-        //}
-        //if (right)
-        //{
-        //    Destroy(right.gameObject);
-        //}
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Vector2 pozycja;
-    //private GameObject leftObj;
-    //private GameObject rightObj;
+    bool kierunek;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //leftObj = GameObject.Find("Left");
-        //rightObj = GameObject.Find("Right");
+ 
     }
 
     // Update is called once per frame
@@ -21,31 +21,33 @@ public class Movement : MonoBehaviour
         if (Input.GetKeyDown("left"))
         {
             transform.position = new Vector3(-2f,0.5f,0);
+            kierunek = true;
         }
 
         if (Input.GetKeyDown("right"))
         {
             transform.position = new Vector3(2f, 0.5f, 0);
+            kierunek = false;
         }
 
     }
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Respawn"))
-    //    {
-    //        Destroy(other.gameObject);
-    //    }
-    //}
-   // private void OnTriggerEnter2D(Collider2D other)
-   // {
-   //     if (Input.GetKeyDown("left"))
-   //     {
-   //         Destroy(other.gameObject);
-   //     }
-   //     if (Input.GetKeyDown("right"))
-   //     {
-   //         Destroy(rightObj);
-   //     }
-   // }
-
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("left") && kierunek == true)
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("left") && kierunek == false)
+        {
+            Debug.Log("GameOver");
+        }
+        if (other.gameObject.CompareTag("right") && kierunek == false)
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("right")&& kierunek == true)
+        {
+            Debug.Log("GameOver");
+        }
+    }
 }
